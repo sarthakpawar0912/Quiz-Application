2bbb96f035e62d21f72174d193faf29b9efe0119
Sarthak Pawar <pawarsr06@gmail.com>
2025-02-19 09:56:31 +0530

    Updated changes in DTO's

--- src/main/java/com/Quiz_Application/Dto/QuestionResponse.java
+++ src/main/java/com/Quiz_Application/Dto/QuestionResponse.java
@@ -12,19 +12,15 @@ public class QuestionResponse {
     }
     public QuestionResponse() {
     }
-
     public Long getQuestionId() {
         return questionId;
     }
-
     public void setQuestionId(Long questionId) {
         this.questionId = questionId;
     }
-
     public String getSelectedOption() {
         return selectedOption;
     }
-
     public void setSelectedOption(String selectedOption) {
         this.selectedOption = selectedOption;
     }
