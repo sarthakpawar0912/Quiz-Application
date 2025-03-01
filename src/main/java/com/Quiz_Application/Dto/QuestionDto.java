6f93700fd30f54391f1c2a7b63920671df2a2b72
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-01 09:45:18 +0530

    Updated features and fixed issues

--- src/main/java/com/Quiz_Application/Dto/QuestionDto.java
+++ src/main/java/com/Quiz_Application/Dto/QuestionDto.java
@@ -22,49 +22,62 @@ public class QuestionDto {
         this.optionD = optionD;
         this.correctOption = correctOption;
     }
+
     public QuestionDto() {
     }
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getQuestionText() {
         return questionText;
     }
+
     public void setQuestionText(String questionText) {
         this.questionText = questionText;
     }
+
     public String getOptionA() {
         return optionA;
     }
+
     public void setOptionA(String optionA) {
         this.optionA = optionA;
     }
     public String getOptionB() {
         return optionB;
     }
+
     public void setOptionB(String optionB) {
         this.optionB = optionB;
     }
     public String getOptionC() {
         return optionC;
     }
+
     public void setOptionC(String optionC) {
         this.optionC = optionC;
     }
+
     public String getOptionD() {
         return optionD;
     }
+
     public void setOptionD(String optionD) {
         this.optionD = optionD;
     }
+
     public String getCorrectOption() {
         return correctOption;
     }
+
     public void setCorrectOption(String correctOption) {
         this.correctOption = correctOption;
     }
+
 }
