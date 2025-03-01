5fcc05891c80475a0e1bed0b5672b6cfe1024858
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-01 09:51:29 +0530

    made some changes in Repositories

--- src/main/java/com/Quiz_Application/Repository/QuestionRepository.java
+++ src/main/java/com/Quiz_Application/Repository/QuestionRepository.java
@@ -1,7 +1,8 @@
 package com.Quiz_Application.Repository;
-
 import com.Quiz_Application.Entities.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question,Long> {
+public interface QuestionRepository extends JpaRepository<Question,Long>
+{
+
 }
