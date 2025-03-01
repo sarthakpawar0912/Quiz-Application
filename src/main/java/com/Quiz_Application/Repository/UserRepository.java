f1f5f001de65da9cc322f46f4851473bde2b538f
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-01 09:52:38 +0530

    made some changes in Repositories

--- src/main/java/com/Quiz_Application/Repository/UserRepository.java
+++ src/main/java/com/Quiz_Application/Repository/UserRepository.java
@@ -5,12 +5,13 @@ import com.Quiz_Application.Entities.User;
 import com.Quiz_Application.Enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByRole(UserRole role);
+
     Optional<User> findByEmail(String email);
+
     Boolean existsByEmail(String email);
 }
