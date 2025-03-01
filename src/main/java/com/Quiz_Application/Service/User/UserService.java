8dcb25b20d590e026aba0eb84869086a6fd45025
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-01 09:54:29 +0530

    made some changes in Services Repository

--- src/main/java/com/Quiz_Application/Service/User/UserService.java
+++ src/main/java/com/Quiz_Application/Service/User/UserService.java
@@ -1,8 +1,10 @@
 package com.Quiz_Application.Service.User;
 import com.Quiz_Application.Entities.User;
-
-public interface UserService {
+public interface UserService
+{
     User createUser(User user);
+
     boolean hasUserWithEmail(String email);
+
     User login(User user);
 }
