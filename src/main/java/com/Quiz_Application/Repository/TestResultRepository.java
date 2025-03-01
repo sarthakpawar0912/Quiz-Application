68714b45a50e09e43a54a5518c195226ed92777d
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-01 09:52:15 +0530

    made some changes in Repositories

--- src/main/java/com/Quiz_Application/Repository/TestResultRepository.java
+++ src/main/java/com/Quiz_Application/Repository/TestResultRepository.java
@@ -2,10 +2,8 @@ package com.Quiz_Application.Repository;
 
 import com.Quiz_Application.Entities.TestResult;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-
-public interface TestResultRepository extends JpaRepository<TestResult,Long> {
-
+public interface TestResultRepository extends JpaRepository<TestResult,Long>
+{
     List<TestResult> findAllByUserId(Long userId);
 }
